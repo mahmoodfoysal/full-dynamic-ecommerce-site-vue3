4b098fe8a4dc7233a227928e9a68a01c57c04597
mahmoodfoysal <foysalcse033@gmail.com>
2024-05-17 20:42:40 +0600

    update new server link

--- src/API/All_API.js
+++ src/API/All_API.js
@@ -13,7 +13,7 @@ export default function getDataFromCentralApiFile() {
     // get api for card category image for home page 
     const GetCatImg = async () => {
         catImgData.value = []
-        const url = 'http://localhost:5000/squreCardForHome';
+        const url = 'https://kitkat-ecommerce-server.onrender.com/squreCardForHome';
         try {
             const res = await axios(url);
             const allCatImages = res.data;
@@ -45,7 +45,7 @@ export default function getDataFromCentralApiFile() {
     // get all categories for navbar dropdown
     const getCategories = async () => {
         categories.value = [];
-        const url = 'http://localhost:5000/categories';
+        const url = 'https://kitkat-ecommerce-server.onrender.com/categories';
         try {
             const res = await axios(url);
             // console.log(res.data);
@@ -59,7 +59,7 @@ export default function getDataFromCentralApiFile() {
     // get api for all products fetching 
     const getProducts = async () => {
       products.value = [];
-      const url = 'http://localhost:5000/products';
+      const url = 'https://kitkat-ecommerce-server.onrender.com/products';
       try {
         const res = await axios(url);
         products.value = res.data;
@@ -73,7 +73,7 @@ export default function getDataFromCentralApiFile() {
     // get api for home page brand sliding 
     const getBrands = async () => {
       brands.value = [];
-      const url = 'http://localhost:5000/brands';
+      const url = 'https://kitkat-ecommerce-server.onrender.com/brands';
       try {
         const res = await axios(url);
         brands.value = res.data;
@@ -85,7 +85,7 @@ export default function getDataFromCentralApiFile() {
 
     // this api for create order. when client place his order then store the data in the database 
     const createOrders = async (customerInfo) => {
-      const url = 'http://localhost:5000/orders';
+      const url = 'https://kitkat-ecommerce-server.onrender.com/orders';
       orders.value = [];
       try {
         const config = {
