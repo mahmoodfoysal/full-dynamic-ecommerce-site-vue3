ce5f7dc4c7e196a14369f8892abe03e8b8c3dd88
mahmoodfoysal <foysalcse033@gmail.com>
2024-05-19 18:01:13 +0600

    search bar style fix

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import NavBar from '../src/components/shared/NavBar/Navbar.vue';
 import Footer from '../src/components/shared/Footer/Footer.vue';
 import { RouterView } from 'vue-router';
-import {onMounted, ref} from 'vue';
+import { onMounted, ref } from 'vue';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useStore } from './stores/TaskStore';
 import MobileMenu from '../src/components/shared/MobileMenu/MobileMenu.vue';
@@ -42,41 +42,29 @@ const handleMobileSearch = () => {
 </script>
 
 <template>
-<!-- All home section are set here  -->
-<div class="home-section-style">
-  <NavBar
-  @search-products="handleSearch"
-  ></NavBar>
-      <!-- search for mobile menu  -->
-      <div class="d-flex justify-content-center">
-        <form @submit.prevent="handleMobileSearch" class="d-flex search-field-style" role="search">
-                    <input 
-                    v-model="searchData"
-                    class="form-control me-2" 
-                    type="search" 
-                    placeholder="Search" 
+    <!-- All home section are set here  -->
+    <div class="home-section-style">
+        <NavBar @search-products="handleSearch"></NavBar>
+        <!-- search for mobile menu  -->
+        <div class="d-flex justify-content-center">
+            <form @submit.prevent="handleMobileSearch" class="d-flex search-field-style" role="search">
+                <input v-model="searchData" class="form-control me-2" type="search" placeholder="Search"
                     aria-label="Search">
-                    <button 
-                    class="btn" 
-                    type="submit">
+                <button class="btn" type="submit">
                     <span class="material-icons">
-                            search
-                        </span>
-                    </button>
-                </form>
+                        search
+                    </span>
+                </button>
+            </form>
+        </div>
+        <RouterView :searchData="searchData"></RouterView>
+        <button @click="scrollToTop" class="top-scroll-btn">
+            <span class="material-icons">
+                expand_less
+            </span></button>
+        <Footer></Footer>
+        <MobileMenu></MobileMenu>
     </div>
-  <RouterView
-  :searchData="searchData"
-  ></RouterView>
-  <button 
-    @click="scrollToTop"
-    class="top-scroll-btn">
-    <span class="material-icons">
-        expand_less
-    </span></button>
-  <Footer></Footer>
- <MobileMenu></MobileMenu>
-</div>
 </template>
 
 <style scoped>
@@ -110,6 +98,7 @@ const handleMobileSearch = () => {
     color: #1F5DA0;
     border: 2px solid #1F5DA0;
 }
+
 .top-scroll-btn:hover {
     background: #1F5DA0;
     border: 2px solid #FFF;
@@ -150,91 +139,93 @@ const handleMobileSearch = () => {
 @media only screen and (max-width: 2560px) {
     .search-field-style {
         display: none;
-}
+    }
 }
 
 @media only screen and (max-width: 1920px) {
     .search-field-style {
         display: none;
-}
+    }
 }
 
 @media only screen and (max-width: 1440px) {
     .search-field-style {
         display: none;
-}
+    }
 }
 
 @media only screen and (max-width: 1024px) {
     .search-field-style {
         display: none;
-}
+    }
 }
 
 @media only screen and (max-width: 768px) {
     .search-field-style {
-    position: fixed;
-    top: 16px;
-    z-index: 2000;
-    display: block;
+        position: fixed;
+        top: 16px;
+        z-index: 2000;
+        display: block;
+    }
+
+    .search-field-style button {
+        width: 55px;
+        height: 35px;
+        background: #1F5DA0;
+        border: none;
+        position: absolute;
+        right: 10px;
+        top: 2px;
+    }
+
+    .search-field-style button span {
+        color: #FFFFFF;
+    }
+
+    .search-field-style input {
+        width: 400px;
+        height: 100%;
+        background: white;
+        /* border: 3px solid #1F5DA0; */
+        border: 1px solid white;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 500px;
+        color: black;
+    }
 }
 
-.search-field-style button {
-    width: 55px;
-    height: 35px;
-    background: #1F5DA0;
-    border: none;
-    position: absolute;
-    right: 10px;
-    top: 2px;
-}
-
-.search-field-style button span {
-    color: #FFFFFF;
-}
-
-.search-field-style input {
-    width: 400px;
-    height: 100%;
-    background: white;
-    /* border: 3px solid #1F5DA0; */
-    border: 1px solid white;
-    font-family: 'Poppins', sans-serif;
-    font-weight: 500px;
-    color: black;
-}
-}
 @media only screen and (max-width: 540px) {
     .search-field-style {
         position: fixed;
         top: 20px;
         z-index: 2000;
-    display: block;
-}
+        display: block;
+        margin-left: 8px;
+    }
 
-.search-field-style button {
-    width: 44px;
-    height: 100%;
-    background: gray;
-    border: none;
-    position: absolute;
-    right: 8px;
-    top: 0px;
-}
+    .search-field-style button {
+        width: 44px;
+        height: 100%;
+        background: gray;
+        border: none;
+        position: absolute;
+        right: 8px;
+        top: 0px;
+        display: none;
+    }
 
-.search-field-style button span {
-    color: #FFFFFF;
-}
+    .search-field-style button span {
+        color: #FFFFFF;
+    }
 
-.search-field-style input {
-    width: 100%;
-    height: 33px;
-    background: white;
-    border: 1px solid #1F5DA0;
-    font-family: 'Poppins', sans-serif;
-    font-weight: 500px;
-    color: black;
+    .search-field-style input {
+        width: 140px;
+        height: 33px;
+        background: white;
+        border: 1px solid #1F5DA0;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 500px;
+        color: black;
+    }
 }
-}
-
 </style>
